8ed405acd8ccbf9a211a359d2cbc68b1e01a67ab
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2022-05-08 00:22:06 +0600

    testing setup & data fetched...

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,25 @@ import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { fetchLaunches } from "./features/launch/launchSlice";
 
 function App() {
-	const launch = useAppSelector((state) => state.launch);
-	const dispatch = useAppDispatch();
-	useEffect(() => {
-		dispatch(fetchLaunches());
-	}, [dispatch]);
-	const { loading, launches, error } = launch;
-	console.log(loading, error);
-	return (
-		<div>
-			{launches.map((launch, index) => {
-				return (
-					<div key={index}>
-						<h2>{launch.rocket.rocket_name}</h2>
-					</div>
-				);
-			})}
-		</div>
-	);
+  const launch = useAppSelector((state) => state.launch);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchLaunches());
+  }, [dispatch]);
+  const { launches, loading, error } = launch;
+  console.log(loading, error);
+
+  return (
+    <div>
+      {launches.map((item, index) => {
+        return (
+          <div key={index}>
+            <h2>{item.rocket.rocket_name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
